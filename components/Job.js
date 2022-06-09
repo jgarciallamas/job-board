0dce1ff2d92f02791e8b4744a5f2baaeff3de764
Jose <jgarciallamas@yahoo.es>
2022-06-09 13:41:57 +0200

    Create a dashboard so that companies can see all jobs posted

--- components/Job.js
+++ components/Job.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const Job = ({ job }) => {
+const Job = ({ job, isDashboard }) => {
   return (
     <div className="mb-4 mt-20 pl-16 pr-16">
       <Link href={`/job/${job.id}`}>
@@ -8,6 +8,16 @@ const Job = ({ job }) => {
       </Link>
       <h2 className="text-base font-normal mt-3">{job.description}</h2>
       <div className="mt-4">
+        {isDashboard && job.published && (
+          <span className="bg-black text-white uppercase text-sm p-2 mr-5">
+            ✅ Published
+          </span>
+        )}
+        {isDashboard && !job.published && (
+          <span className="bg-black text-white uppercase text-sm p-2 mr-5">
+            ❌ Unpublished
+          </span>
+        )}
         <h4 className="inline">Posted by</h4>
         <div className="ml-3 -mt-6 inline">
           <span>
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,3 +1,48 @@
-export default function Dashboard() {
-  return <h2>Dasboard page</h2>;
+import prisma from "lib/prisma";
+import { getSession, useSession } from "next-auth/react";
+import { getUser, getJobsPosted } from "lib/data";
+import Jobs from "components/Jobs";
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  let user = await getUser(session.user.id, prisma);
+  user = JSON.parse(JSON.stringify(user));
+
+  let jobs = await getJobsPosted(user.id, prisma);
+  jobs = JSON.parse(JSON.stringify(jobs));
+
+  return {
+    props: {
+      jobs,
+      user,
+    },
+  };
+}
+
+export default function Dashboard({ jobs, user }) {
+  const { data: session, status } = useSession();
+
+  return (
+    <div className="mt-10">
+      <div className="text-center p-4 m-4">
+        <h2 className="mb-10 text-4xl font-bold">Dashboard</h2>
+        {user.company && (
+          <span className="bg-black text-white uppercase text-sm p-2 ">
+            Company
+          </span>
+        )}
+        {session && (
+          <>
+            {user.company && (
+              <p className="mt-10 mb-10 text-2xl font-normal">
+                all the jobs you posted
+              </p>
+            )}
+          </>
+        )}
+      </div>
+      <Jobs jobs={jobs} isDashboard={true} />
+    </div>
+  );
 }
