d317d79a73edc51258186b3013b2fb2eed23d7d0
Jose <jgarciallamas@yahoo.es>
2022-06-11 18:00:47 +0200

    Show the applicants in the company dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,6 +3,7 @@ import { getSession, useSession } from "next-auth/react";
 import { getUser, getJobsPosted, getApplications } from "lib/data";
 import Jobs from "components/Jobs";
 import Link from "next/link";
+import Job from "./job/[id]";
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
@@ -48,7 +49,43 @@ export default function Dashboard({ jobs, user, applications }) {
         )}
       </div>
       {user.company ? (
-        <Jobs jobs={jobs} isDashboard={true} />
+        // <Jobs jobs={jobs} isDashboard={true} />
+        <div>
+          {jobs.map((job, index) => (
+            <>
+              <Job key={index} job={job} isDashboard={true} />
+
+              <div className="mb-4 mt-20">
+                <div className="pl-16 pr-16 -mt-6">
+                  {job.applications.length === 0 ? (
+                    <p className="mb-10 text-2xl font-normal">
+                      No applications so far 😞
+                    </p>
+                  ) : (
+                    <p className="mb-10 text-2xl font-normal">
+                      {job.applications.length} applications
+                    </p>
+                  )}
+
+                  {job.applications?.map((application, index) => (
+                    <>
+                      <h2 className="text-base font-normal mt-3">
+                        <span className="text-base font-bold mt-3 mr-3">
+                          {application.author.name}
+                        </span>
+                        {application.author.email}
+                      </h2>
+                      <p className="text-lg font-normal mt-2 mb-3">
+                        {application.coverletter}
+                      </p>
+                      <hr />
+                    </>
+                  ))}
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
       ) : (
         <>
           {applications.map((application, index) => {
